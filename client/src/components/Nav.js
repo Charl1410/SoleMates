cc6147552b4763fab53d83cfd4dc4ef358e88f9d
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-03-09 20:37:32 +0000

    nav.js

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -31,19 +31,19 @@ export default function Nav () {
             <li><a>Item 1</a></li>
             <li tabIndex={0}>
               <a>
-                Parent
+            Brands
                 <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
               </a>
               <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
+                <li><a>Nike</a></li>
+                <li><a>Adidas</a></li>
               </ul>
             </li>
             <li><a>Item 3</a></li>
           </ul>
         </div>
 
-        
+
         <div className="flex-none">
     <div className="dropdown dropdown-end">
       <label tabIndex={0} className="btn btn-ghost btn-circle">
