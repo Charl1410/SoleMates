063ed5f42e7139f93b23479cd76073c3571a23b6
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-03-10 21:34:00 +0000

    adding personalsation to nav

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -24,7 +24,7 @@ export default function Nav () {
             <li><a>Item 3</a></li>
           </ul>
         </div>
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+        <a className="btn btn-ghost normal-case text-xl">SOLE MATES</a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
@@ -43,8 +43,10 @@ export default function Nav () {
         </ul>
       </div>
 
+      
+
       <div className="navbar-end">
-      <div className="dropdown dropdown-end ">
+<div className="dropdown dropdown-end ">
           <label tabIndex={0} className="btn btn-ghost btn-circle">
             <div className="indicator">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
@@ -62,7 +64,8 @@ export default function Nav () {
               </div>
             </div>
           </div>
-       </div>      </div>
+       </div>      
+       </div>
 
 
 
