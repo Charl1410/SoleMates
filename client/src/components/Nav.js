718d846a0bd421a346cc8132210f5f8e9439e92f
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-03-13 21:10:58 +0000

    z index

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -38,7 +38,7 @@ export default function Nav () {
               Shop Brands
               <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
             </a>
-            <ul className="p-2 bg-white z-10">
+            <ul className="p-2 bg-white">
               <li ><a>Timberland</a></li>
               <li><a>Nike</a></li>
               <li><a>Adidas</a></li>
@@ -54,10 +54,10 @@ export default function Nav () {
 
           <li tabIndex={0}>
             <a>
-              Shop category
+              Shop Category
               <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
             </a>
-            <ul className="p-2 bg-white">
+            <ul className="p-2 bg-white z-10">
               <li ><a>Boots</a></li>
               <li><a>Trainers</a></li>
               <li><a>Slippers</a></li>
