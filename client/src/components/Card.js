5e1cef92ca8bd2fc3dade0f56f2c026e665d2b30
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-03-13 13:07:15 +0000

    added flex and padding in browse all page

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -7,7 +7,7 @@ const Card = ( props ) => {
   const {title, image, price} = props;
 
   return (
-  <div className="card w-96 bg-base-100 shadow-xl">
+  <div className="card w-96 bg-base-100 shadow-xl m-5">
     <figure><img src={`${image}`} alt={title} /></figure>
     <div className="card-body">
       <h2 className="card-title">
