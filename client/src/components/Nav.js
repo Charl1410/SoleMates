c9adaf9ef65cfb496301b02ab362664ddc2ca678
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-03-13 17:38:01 +0000

    added more seed data for browse  display

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -32,7 +32,7 @@ export default function Nav () {
           <li><a href="/login">Log In</a></li>
           <li tabIndex={0}>
             <a>
-              Brands
+              Shop Brands
               <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
             </a>
             <ul className="p-2">
@@ -49,12 +49,12 @@ export default function Nav () {
           <label tabIndex={0} className="btn btn-ghost btn-circle">
             <div className="indicator">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-              <span className="badge badge-sm indicator-item">Number of items</span>
+              <span className="badge badge-sm indicator-item">N</span>
             </div>
           </label>
           <div tabIndex={0} className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
             <div className="card-body">
-              <span className="font-bold text-lg">Number of items</span>
+              <span className="font-bold text-lg">X items</span>
               <span className="text-info">Subtotal:</span>
               <div className="card-actions">
                 <a href="/basket">
