8fdeb6d813be134592f96e3d138bd5bb04936b44
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-03-14 20:22:12 +0000

    basket card styling

--- client/src/components/SingleProduct.js
+++ client/src/components/SingleProduct.js
@@ -3,8 +3,8 @@ import React from 'react';
 const SingleProduct = ({ title, image, description, price, onAddToCart }) => {
   return (
     <div className="product flex flex-col justify-center items-center text-center">
-      <div className='image-container w-300 h-300 flex flex-col justify-center items-center text-center'> 
-        <img className='w-3/12' src={image} alt={title} />
+      <div className='image-container w-96 h-96 flex flex-col justify-center items-center text-center'> 
+        <img src={image} alt={title} />
       </div>
       <h2 className="text-4xl font-extrabold dark:text-white text-center m-5">{title}</h2>
       <p className='m-2'>{description}</p>
