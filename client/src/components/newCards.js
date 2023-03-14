2ef5057f099e593a43d6433e178f45a7e06fa621
tiiingaling <93777306+tiiingaling@users.noreply.github.com>
2023-03-14 02:39:03 +0000

    log in page input type changed to password

--- client/src/components/newCards.js
+++ client/src/components/newCards.js
@@ -5,29 +5,34 @@ const newCards = ( props ) => {
   const {title, image, price, _id, onAddToCart} = props;
 
   return (
-   <div className="card w-96 bg-base-100 shadow-xl m-5 w-80 transition duration-500 hover:scale-125 ">
+    <div className="card w-96 bg-base-100 shadow-xl m-5 w-80 transition duration-500 hover:scale-125 ">
       <Link to={`/product/${_id}`}>
         <figure>
           <img src={`${image}`} alt={title} />
         </figure>
       </Link>
+      
       <div className="card-body">
         <h2 className="card-title">
           <Link to={`/product/${_id}`}>
             {title}
           </Link>
+    
+          <div className="badge badge-secondary">NEW</div>
         </h2>
-        <div className="badge badge-secondary">NEW</div>
+       
         <div className="card-actions justify-end">
+        <div className="badge badge-outline">£{price}</div>
           <button className="btn btn-primary " onClick={onAddToCart}>
             Add to Cart
           </button>
-          <div className="badge badge-outline">£{price}</div> 
+           
         </div>
       </div>
     </div>
   )
 }
+
 
 export default newCards
 
