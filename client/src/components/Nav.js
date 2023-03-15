bda798d198a06a8e403990fe705ed8f1ea7e6eaf
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-03-15 14:00:34 +0000

    moved log in and made white background for phone view drop down

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -13,14 +13,14 @@ export default function Nav () {
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </label>
+    
           <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-            <li><a>Log In</a></li>
             <li tabIndex={0}>
               <a className="justify-between">
                 Shop Brands
                 <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
               </a>
-              <ul className="p-2">
+              <ul className="p-2 bg-white z-10">
               <li ><a>Timberland</a></li>
               <li><a>Nike</a></li>
               <li><a>Adidas</a></li>
@@ -31,7 +31,7 @@ export default function Nav () {
               <li><a>Veja</a></li>
               </ul>
             </li>
-
+          
             <li tabIndex={0}>
             <a>
               Shop Category
