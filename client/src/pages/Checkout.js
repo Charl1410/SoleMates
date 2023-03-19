3b281398ef5caaaf1acb1e38361d9a1fc81e955a
tiiingaling <93777306+tiiingaling@users.noreply.github.com>
2023-03-19 12:21:44 +0000

    added promo code and total checkout

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -10,7 +10,6 @@ import {
 } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 
-
 import { useMutation } from "@apollo/client";
 import { CHECKOUT } from "../utils/mutations";
 
@@ -60,75 +59,108 @@ const CheckoutForm = () => {
   };
 
   return (
-<div className="card-container bg-neutral-100 w-full flex justify-center items-center">
-        <div className="card  w-6/12 bg-base-100 shadow-xl flex justify-center items-center m-10 p-5" >
-              <form onSubmit={handlePayment} className='w-full'>
+    <div className="flex flex-auto">
+      <div className="card-container bg-neutral-100 w-full flex justify-center items-center">
+        <div className="card  w-6/12 bg-base-100 shadow-xl flex justify-center items-center m-10 p-5">
+          <p>Total: £{total}</p>
+          <div className="form-row mb-10">
+            <label htmlFor="address">Promo Code: </label>
+            <input
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              type="text"
+              id="promo-code"
+            />
+            <div className="flex justify-center ">
+              <button class="btn btn-primary m-5">Add Promo</button>
+            </div>
+          </div>
+        </div>
 
-                    <div className="form-row mb-5 ">
-                      <label className='border-solid' placeholder="hello" htmlFor="name">Name:  </label>
-                      <input placeholder="John" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" id="name" />
-                    </div>
+        <div className="card-container bg-neutral-100 w-full flex justify-center items-center">
+          <div className="card  w-6/12 bg-base-100 shadow-xl flex justify-center items-center m-10 p-5">
+            <form onSubmit={handlePayment} className="w-full">
+              <div className="form-row mb-5 ">
+                <label
+                  className="border-solid"
+                  placeholder="hello"
+                  htmlFor="name"
+                >
+                  Name:{" "}
+                </label>
+                <input
+                  placeholder="John"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  type="text"
+                  id="name"
+                />
+              </div>
 
-                    <div className="form-row mb-10">
-                      <label htmlFor="address">Promo Code:  </label>
-                      <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" id="promo-code" />
-                    </div>
+              <div className="form-row mb-5">
+                <label htmlFor="address">Address line 1: </label>
+                <input
+                  placeholder="21 St Johns Close"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  type="text"
+                  id="address"
+                />
+              </div>
 
-                    <div className="form-row mb-5">
-                      <label htmlFor="address">Address line 1:  </label>
-                      <input placeholder="21 St Johns Close" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" id="address" />
-                    </div>
+              <div className="form-row mb-5">
+                <label htmlFor="address">Address line 2: </label>
+                <input
+                  placeholder=""
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  type="text"
+                  id="address"
+                />
+              </div>
 
+              <div className="form-row mb-5">
+                <label htmlFor="address">City: </label>
+                <input
+                  placeholder="Birmingham"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  type="text"
+                  id="address"
+                />
+              </div>
 
-                    <div className="form-row mb-5">
-                      <label htmlFor="address">Address line 2:  </label>
-                      <input placeholder="" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" id="address" />
-                    </div>
+              <div className="form-row mb-10">
+                <label htmlFor="address">Post Code: </label>
+                <input
+                  placeholder="B93 0FE"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  type="text"
+                  id="address"
+                />
+              </div>
 
+              <div className="form-row mb-5">
+                <label htmlFor="card-number">Card Number:</label>
+                <CardNumberElement id="card-number" />
+              </div>
+              <div className="form-row mb-5">
+                <label htmlFor="card-expiry">Expiration Date:</label>
+                <CardExpiryElement id="card-expiry" />
+              </div>
+              <div className="form-row mb-5">
+                <label htmlFor="card-cvc">CVC:</label>
+                <CardCvcElement id="card-cvc" />
+              </div>
 
-                    <div className="form-row mb-5">
-                      <label htmlFor="address">City:  </label>
-                      <input placeholder="Birmingham" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" id="address" />
-                    </div>
-
-
-                    <div className="form-row mb-10">
-                      <label htmlFor="address">Post Code:  </label>
-                      <input placeholder="B93 0FE" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" id="address" />
-                    </div>
-                    
-                   
-
-                        <div className="form-row mb-5">
-                      <label htmlFor="card-number">Card Number:</label>
-                      <CardNumberElement id="card-number" />
-                    </div>
-                    <div className="form-row mb-5">
-                      <label htmlFor="card-expiry">Expiration Date:</label>
-                      <CardExpiryElement id="card-expiry" />
-                    </div>
-                    <div className="form-row mb-5">
-                      <label htmlFor="card-cvc">CVC:</label>
-                      <CardCvcElement id="card-cvc" />
-                      
-                    </div>
-
-                    <div className="flex justify-center items-center"> 
-                      <button class="btn btn-primary m-5">Confirm Payment</button>
-                    </div>
-              </form>
+              <div className="flex justify-center items-center">
+                <button class="btn btn-primary m-5">Confirm Payment</button>
+              </div>
+            </form>
 
             {error && <div className="error">{error}</div>}
-            {paymentConfirmed && <div className="success">Payment confirmed!</div>}
-
-    
-    
+            {paymentConfirmed && (
+              <div className="success">Payment confirmed!</div>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
-
-   
-
-    
-  </div>
   );
 };
 
