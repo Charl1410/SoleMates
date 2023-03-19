a73942905a2f9a9867d5d26615fb535c5166c30a
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-03-19 00:43:52 +0000

    fixed the image spelling

--- client/src/components/HeaderFont.js
+++ client/src/components/HeaderFont.js
@@ -4,7 +4,7 @@ import HeaderText from '../images/headerText.png';
 const Header = () => {
   return (
     <div className='flex justify-start items-center'>
-      <img className="w-4/12" src={HeaderText} alt="My Image" />
+      <img className="w-2/12" src={HeaderText} alt="text" />
     </div>
   );
 };
