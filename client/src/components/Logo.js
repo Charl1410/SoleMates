8304c9ea293549defe6bb10175404c18a2dd1d2c
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-03-19 00:08:19 +0000

    logo component and pull in'

---
+++ client/src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import myLogo from '../images/logo.png';
+
+const Logo = () => {
+  return (
+    <div>
+      <img src={myLogo} alt="My Image" />
+    </div>
+  );
+};
+
+export default Logo;
